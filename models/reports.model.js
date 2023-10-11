7a0c8fc87f46b5ec81a1af4755ea11290d0d34f5
ArturoSR <a01275427@tec.mx>
2023-10-11 01:01:52 -0600

    a

--- models/reports.model.js
+++ models/reports.model.js
@@ -16,7 +16,7 @@ module.exports = class ReportsModel{
     }
 
     static fetchLeads(){
-        return db.execute('SELECT createdAt, idLeads FROM leads');
+        return db.execute('SELECT createdAt, idLead FROM leads');
     }
 
 
